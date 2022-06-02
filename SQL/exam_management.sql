e0c9411ab6ce5fabaf70a7983f19c71dae249f46
PROFESSOR <92365820+X-PROFESSOR@users.noreply.github.com>
2022-06-02 10:11:17 +0530

    Add files via upload

---
+++ SQL/exam_management.sql
@@ -0,0 +1,289 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2022 at 06:31 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exam_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_class`
+--
+
+CREATE TABLE `add_class` (
+  `class_id` int(11) NOT NULL,
+  `class_name` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_course`
+--
+
+CREATE TABLE `add_course` (
+  `course_id` int(100) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `create_time` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_exam`
+--
+
+CREATE TABLE `add_exam` (
+  `exam_id` int(100) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `exam_time_limit` varchar(100) NOT NULL,
+  `total_question` varchar(100) NOT NULL,
+  `correct` varchar(100) NOT NULL,
+  `wrong` varchar(100) NOT NULL,
+  `exam_title` varchar(100) NOT NULL,
+  `exam_date` varchar(100) NOT NULL,
+  `exam_time` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_question`
+--
+
+CREATE TABLE `add_question` (
+  `question_id` int(100) NOT NULL,
+  `exam_title` varchar(100) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `exam_qs_id` int(100) NOT NULL,
+  `question` varchar(100) NOT NULL,
+  `ans_1` varchar(100) NOT NULL,
+  `ans_2` varchar(100) NOT NULL,
+  `ans_3` varchar(100) NOT NULL,
+  `ans_4` varchar(100) NOT NULL,
+  `correct_answer` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_student`
+--
+
+CREATE TABLE `add_student` (
+  `std_id` int(100) NOT NULL,
+  `std_name` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `year` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_reg`
+--
+
+CREATE TABLE `admin_reg` (
+  `adm_id` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `emailid` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `special_token` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_reg`
+--
+
+INSERT INTO `admin_reg` (`adm_id`, `image`, `full_name`, `contact`, `emailid`, `password`, `special_token`) VALUES
+(2, 'uploads/EAztFwu0Hr.jpg', 'Nilesh Mukherjee', '8754963235', 'admin@email', '1234', 'MthPNTKv0n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign_course`
+--
+
+CREATE TABLE `assign_course` (
+  `assign_id` int(100) NOT NULL,
+  `class_name` varchar(100) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `create_time` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_answers`
+--
+
+CREATE TABLE `exam_answers` (
+  `id` int(100) NOT NULL,
+  `std_name` varchar(100) NOT NULL,
+  `std_email` varchar(100) NOT NULL,
+  `exam_title` varchar(100) NOT NULL,
+  `question` varchar(100) NOT NULL,
+  `answered` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `std_exam_status`
+--
+
+CREATE TABLE `std_exam_status` (
+  `status_id` int(100) NOT NULL,
+  `std_name` varchar(100) NOT NULL,
+  `std_email` varchar(100) NOT NULL,
+  `exam_name` varchar(100) NOT NULL,
+  `attendence_status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `add_class`
+--
+ALTER TABLE `add_class`
+  ADD PRIMARY KEY (`class_id`);
+
+--
+-- Indexes for table `add_course`
+--
+ALTER TABLE `add_course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `add_exam`
+--
+ALTER TABLE `add_exam`
+  ADD PRIMARY KEY (`exam_id`);
+
+--
+-- Indexes for table `add_question`
+--
+ALTER TABLE `add_question`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `add_student`
+--
+ALTER TABLE `add_student`
+  ADD PRIMARY KEY (`std_id`);
+
+--
+-- Indexes for table `admin_reg`
+--
+ALTER TABLE `admin_reg`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Indexes for table `assign_course`
+--
+ALTER TABLE `assign_course`
+  ADD PRIMARY KEY (`assign_id`);
+
+--
+-- Indexes for table `exam_answers`
+--
+ALTER TABLE `exam_answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `std_exam_status`
+--
+ALTER TABLE `std_exam_status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `add_class`
+--
+ALTER TABLE `add_class`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `add_course`
+--
+ALTER TABLE `add_course`
+  MODIFY `course_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `add_exam`
+--
+ALTER TABLE `add_exam`
+  MODIFY `exam_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `add_question`
+--
+ALTER TABLE `add_question`
+  MODIFY `question_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+
+--
+-- AUTO_INCREMENT for table `add_student`
+--
+ALTER TABLE `add_student`
+  MODIFY `std_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `admin_reg`
+--
+ALTER TABLE `admin_reg`
+  MODIFY `adm_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `assign_course`
+--
+ALTER TABLE `assign_course`
+  MODIFY `assign_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `exam_answers`
+--
+ALTER TABLE `exam_answers`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `std_exam_status`
+--
+ALTER TABLE `std_exam_status`
+  MODIFY `status_id` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
