3e5066da1ecab6192c526e82801047480444e042
PROFESSOR <92365820+X-PROFESSOR@users.noreply.github.com>
2022-06-24 22:39:04 +0530

    Add files via upload

---
+++ student/js/password_icon.js
@@ -0,0 +1,17 @@
+const togglePasswordEye = '<i class="fa fa-eye toggle-password-eye"></i>';
+const togglePasswordEyeSlash = '<i class="fa fa-eye-slash toggle-password-eye"></i>';
+
+$(togglePasswordEyeSlash).insertAfter('input[type=password]');
+$('input[type=password]').addClass('hidden-pass-input')
+
+$('body').on('click', '.toggle-password-eye', function (e) {
+    let password = $(this).prev('.hidden-pass-input');
+
+    if (password.attr('type') === 'password') {
+        password.attr('type', 'text');
+        $(this).addClass('fa-eye').removeClass('fa-eye-slash');
+    } else {
+        password.attr('type', 'password');
+        $(this).addClass('fa-eye-slash').removeClass('fa-eye');
+    }
+})
